f3e709d1f09e39cf299f23993aee27c73fe4c517
anwarkvzk <anvarvzk521@gmail.com>
2022-07-25 20:13:13 +0530

    Main heading and bg Image added

--- main.py
+++ main.py
@@ -37,7 +37,17 @@ class Face_Recognition_System:
         f_lbl.place(x=1000, y=0, width=500, height=130)
 
 
+        #Background image
+        img3 = Image.open("/home/anvar/Desktop/face_recognition/bgimage.jpg")
+        img3 = img3.resize((1530, 710), Image.ANTIALIAS)
+        self.photoimg3 = ImageTk.PhotoImage(img3)
 
+        bg_img = Label(self.root, image=self.photoimg3)
+        bg_img.place(x=0, y=130, width=1530, height=710)
+
+
+        title_lbl = Label(bg_img, text="FACE RECOGNITION SYSTEM ATTENDENCE SOFTWARE", font=("times new roman", 35, "bold"), bg="white", fg="red")
+        title_lbl.place(x=0, y=0, width=1530, height=45)
 
 
 if __name__ == "__main__":
